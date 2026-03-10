2a3dcc8f736547d6f72d1ad9bb8044606b9d1f84
thelastbucuc <yusufdurak939@gmail.com>
2026-03-10 23:14:54 +0300

    added screen wrapping to ship

--- scenes/Ship/Ship.gd
+++ scenes/Ship/Ship.gd
@@ -6,6 +6,7 @@ extends RigidBody2D
 var thrust: Vector2 = Vector2.ZERO
 var rotation_dir: float = 0
 
+@onready var screensize: Vector2 = get_viewport_rect().size
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -24,3 +25,10 @@ func get_input() -> void:
 	if Input.is_action_pressed("thrust"):
 		thrust = transform.x * engine_power
 	rotation_dir = Input.get_axis("turn_left", "turn_right")
+
+
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	var xform: Transform2D = state.transform
+	xform.origin.x = wrapf(xform.origin.x, 0, screensize.x)
+	xform.origin.y = wrapf(xform.origin.y, 0, screensize.y)
+	state.transform = xform
